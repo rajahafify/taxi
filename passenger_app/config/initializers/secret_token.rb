47a35667f941fc8d2083d77adc37619ec550ef25
Raja Hafify <rajahafify.zaed@gmail.com>
2014-03-26 15:21:08 +0800

    Initial rails setup for passenger app

---
+++ passenger_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PassengerApp::Application.config.secret_key_base = '5346f2cca422e12bc825a11f2f0824f974303c3edbea36afaacdb012e979b81d09aa0f93f001593778cbcdbfc0a9f9f94b98c95545df48fed384405e161a91db'
