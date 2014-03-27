65915e011a55ecfc392cecd86a2675ba08ab050f
Raja Hafify <rajahafify.zaed@gmail.com>
2014-03-27 17:03:47 +0800

    Initial commit for dispatcher app

---
+++ dispatcher_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DispatcherApp::Application.config.secret_key_base = '3a80e25d8b2cbac78f552999e78db259775e59c0c91050eda879dbaa0183f7c4070ed410b618f3531ebe2f81069834586ac24083147f41759c0de222fd648585'
